766f41912eca2f4643b314a76ebaef76a743a8d7
X-Engineer-001 <fabin1790@gmail.com>
2019-01-02 21:31:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,50 +12,61 @@ player={
   xmove:0,
   ymove:0,
   speed:100,
-  move:function(x,y){
-    this.xmove=
-  }
+  move:true,
+  fist:true,
+  communicate:false,
+  
 }
-function Brush(){
-  this.x=Math.random()*700;
-  this.y=Math.random()*700;
+function Brush(x,y){
+  this.x=x;
+  this.y=y;
 }
-function Tree(){
-  this.x=Math.random()*700;
-  this.y=Math.random()*700;
+function Tree(x,y){
+  this.x=x;
+  this.y=y;
 }
-function Spirit(){
-  this.x=Math.random()*700;
-  this.y=Math.random()*700;
+function Spirit(x,y){
+  this.x=x;
+  this.y=y;
+  this.xmove=Math.cos(Math.random()*2*Math.PI);
+  this.ymove=Math.sin(Math.random()*2*Math.PI);
+  this.speed=200;
 }
-function Animal(){
-  this.x=Math.random()*700;
-  this.y=Math.random()*700;
+function Animal(x,y){
+  this.x=x;
+  this.y=y;
   this.xmove=Math.cos(Math.random()*2*Math.PI);
   this.ymove=Math.sin(Math.random()*2*Math.PI);
   this.speed=60;
 }
-function Human(){
-  this.x=Math.random()*700;
-  this.y=Math.random()*700;
+function Human(x,y){
+  this.x=x;
+  this.y=y;
   this.xmove=Math.cos(Math.random()*2*Math.PI);
   this.ymove=Math.sin(Math.random()*2*Math.PI);
   this.speed=100;
 }
-function Poorman(){
-  this.x=Math.random()*700;
-  this.y=Math.random()*700;
+function Poorman(x,y){
+  this.x=x;
+  this.y=y;
   this.xmove=Math.cos(Math.random()*2*Math.PI);
   this.ymove=Math.sin(Math.random()*2*Math.PI);
   this.speed=20;
 }
-function Ghost(){
-  this.x=Math.random()*700;
-  this.y=Math.random()*700;
+function Ghost(x,y){
+  this.x=x;
+  this.y=y;
+  this.xmove=Math.cos(Math.random()*2*Math.PI);
+  this.ymove=Math.sin(Math.random()*2*Math.PI);
+  this.speed=200;
 }
-function Corpse(){
-  this.x=Math.random()*700;
-  this.y=Math.random()*700;
+function Corpse(x,y){
+  this.x=x;
+  this.y=y;
+}
+function Conflict(x,y){
+  this.x=x;
+  this.y=y;
 }
 function run(){
   
